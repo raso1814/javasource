1b7f19a01a5ebdc88fa3e5a8b77ce691206f9901
sora <raso180814@gmail.com>
2021-09-30 16:02:35 +0900

    connection

--- jdbc/src/dept/DeptDTO.java
+++ jdbc/src/dept/DeptDTO.java
@@ -1,5 +1,23 @@
 package dept;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
 public class DeptDTO {
+
+	//테이블 모양과 똑같이 생성
+	
+	private int deptNo;
+	private String dname;
+	private String loc;
 
 }
