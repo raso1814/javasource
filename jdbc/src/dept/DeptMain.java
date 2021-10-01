d8474bd2c72efa701d506f563ec00e92496db8e1
sora <raso180814@gmail.com>
2021-10-01 11:22:09 +0900

    메뉴구성

--- jdbc/src/dept/DeptMain.java
+++ jdbc/src/dept/DeptMain.java
@@ -1,9 +1,17 @@
 package dept;
 
+import java.util.Scanner;
+
 public class DeptMain {
 
 	public static void main(String[] args) {
-	
+	boolean run = false;
+Scanner sc = new Scanner(System.in);
+		
+		while(run) {
+			
+			
+		}
 
 	}
 
